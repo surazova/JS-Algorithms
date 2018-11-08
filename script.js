d1eb5492513bca624b45dc47a62f76c42969480e
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-08 19:12:27 +0000

    Reverse a string

---
+++ script.js
@@ -0,0 +1,11 @@
+//Reverse a String 
+
+function reverseString(str) {
+  let newStr = str.split('').reverse().join('')
+
+  // add for loop 
+  console.log(newStr)
+  return str;
+}
+
+reverseString("howdy");
