3b7ccc838465bbab1af4c94a8b7ce09b145932cd
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-09 19:13:32 +0000

    updates

---
+++ tree.js
@@ -0,0 +1,34 @@
+function tree(n) {
+  if (n < 1) {
+    return n
+  }
+
+  else {
+    return n * tree(n + 1)
+  }
+}
+
+tree(2)
+
+
+function factorial(n, accumulator) {
+  if (n === 0) {
+    return accumulator
+  }
+  return factorial(n - 1, n * accumulator)
+}
+factorial(5, 1) //==>> 120
+
+
+function makeLine(length) {
+  var line = "";
+  for (var i = 1; i <= length; i++) {
+    for (var j = 1; j <= i; j++) {
+      line += "*";
+
+    }
+    line += "\n";
+  }
+  return line;
+}
+console.log(makeLine(5));
