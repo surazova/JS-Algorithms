ecfaf36e7c711c96567f5eb2e9804e53c72c2c17
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-15 19:16:12 +0000

    Make tree

--- tree.c
+++ tree.c
@@ -1,1 +1,36 @@
-void 
+#include <stdio.h>
+
+int j, count, t = 0;
+
+void tree(int);
+
+int main() {
+  int x;
+    system("cls");
+    printf("Enter number of lines: ");
+    scanf("%d", &x);
+  count = x;
+  tree(x);
+    getchar();
+}
+
+void tree(int x)
+{
+  int i;
+  for(i = x + x -1; i > 0; i --)
+  printf("*");
+  x--;
+  
+  printf("\n");
+  j = 0;
+    while(j <= t)
+    {
+      printf(" ");
+      j++;
+    }
+    if(t < count)
+    t++;
+    if(x > 0)
+    tree(x);
+}
+
