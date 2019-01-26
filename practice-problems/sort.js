88bc79430ec5e4a3e02f7814a319b619bc133568
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-26 17:53:58 +0000

    Added two sorting methods for numbers

--- practice-problems/sort.js
+++ practice-problems/sort.js
@@ -1,8 +1,13 @@
 // Given an array, sort the array 
 // Example: [3, 4, 5, 1, 2] ===> [1, 2, 3, 4, 5]
 
-// Pseudo Code
-// given an array 
-// split up the array
-// loop through the array and place the numbers from least to greatest 
-// place the numbers back together
+
+var unordered = [3, 4, 2, 1, 5]
+unordered.sort();
+
+
+// Another way
+var unsorted = [3, 4, 2, 1, 5]
+unsorted.sort(function(a, b) {
+  return (a - b)
+});
