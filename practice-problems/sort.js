8fc98c3b9698c5cfe19c6c87b9c74058f4bcbfab
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-26 17:48:13 +0000

    Added pseudo code

---
+++ practice-problems/sort.js
@@ -0,0 +1,8 @@
+// Given an array, sort the array 
+// Example: [3, 4, 5, 1, 2] ===> [1, 2, 3, 4, 5]
+
+// Pseudo Code
+// given an array 
+// split up the array
+// loop through the array and place the numbers from least to greatest 
+// place the numbers back together
