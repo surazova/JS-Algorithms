ac2d1cd1f75e4ab1cc42fdbecb593bdfb2736460
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-27 21:06:15 +0000

    Edit

--- practice-problems/sort.js
+++ practice-problems/sort.js
@@ -11,3 +11,17 @@ var unsorted = [3, 4, 2, 1, 5]
 unsorted.sort(function(a, b) {
   return (a - b)
 });
+
+// Given an array, sort the array 
+// Example: [3, 4, 5, 1, 2] ===> [1, 2, 3, 4, 5]
+
+
+var unordered = [3, 4, 2, 1, 5]
+unordered.sort();
+
+
+// Another way
+var unsorted = [3, 4, 2, 1, 5]
+unsorted.sort(function(a, b) {
+  return (a - b)
+});
