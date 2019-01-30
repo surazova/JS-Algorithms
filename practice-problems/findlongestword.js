d7500e976473c5fb71a68a66a7c511e21d45d082
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-30 18:41:12 +0000

    Created a find longest word function using str.split

--- practice-problems/findlongestword.js
+++ practice-problems/findlongestword.js
@@ -1,10 +1,24 @@
 // Find the longest word in a string
 function findLongestWordLength(str) {
 
+    // 1: Split the string into words
+    var strSplit = str.split(' ');
+
+    // 2: Create a varibalbe that holds the length of the longest word  
+    var longWord = 0;
+
+    // 3: Create a for loop that runs through the words and compares them to each other 
+    for (var i = 0; i < strSplit.length; i++) {
+        if (strSplit[i].length > longWord) {
+            longWord = strSplit.length;
+        }
+    }
+
+
+    return longWord;
 }
 
-function findLongestWordLength(str) {
-    return str.length;
-}
+
+
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
