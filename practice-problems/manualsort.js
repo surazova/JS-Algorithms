09e9de79f43ea6f4f277c056eaff29cbbdecf4bf
Sitora Urazova <sitoraurazova@gmail.com>
2019-01-31 19:10:24 +0000

    Created array to manually sort

--- practice-problems/manualsort.js
+++ practice-problems/manualsort.js
@@ -1,27 +1,28 @@
 // Given an array, sort the array manually (without using .sort())
 
-// Using the bubble sort method: 
+// Using the bubble sort method: Comparing two numbers and moving the lowest to the left 
 
-// array to sort
+// 1: Create the array to sort
 var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
-// swap function helper
+// 2: Array i: temporary array
+//    Array j: temporary array 
+
 function swap(array, i, j) {
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+  var hold = array[i];
+  array[i] = array[j];
+  array[j] = hold;
 }
 
-// be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
-function bubbleSortBasic(array) {
-    for (var i = 0; i < array.length; i++) {
-        for (var j = 1; j < array.length; j++) {
-            if (array[j - 1] > array[j]) {
-                swap(array, j - 1, j);
-            }
-        }
+// 3: Function for moving through the numbers and swapping out the lowest 
+function sortArray(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
     }
-    return array;
+  }
+  return array;
 }
-
-console.log(bubbleSortBasic(array.slice())); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(sortArray(array.slice()));
