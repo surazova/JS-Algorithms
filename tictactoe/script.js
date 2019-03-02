ed50b6bdaf177d3d6c1ec58bea576960d1f689e3
Sitora Urazova <sitoraurazova@gmail.com>
2019-03-02 19:01:48 +0000

    Added winning combinations

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -5,7 +5,8 @@ var points1 = 0; // Player 1s points
 var points2 = 0; // Player 2s points 
 var size = 3;
 
-
+// Draws a tic tac toe board (n x n table) 
+// n = 3
 function drawBoard() {
   var parent = document.getElementById("game");
   var counter = 1;
@@ -22,4 +23,22 @@ function drawBoard() {
     }
     parent.appendChild(row);
   }
+}
+
+// Winning combinations 
+// Horizontal: [1, 2,3] [4, 5,6] [7, 8,9]
+// Vertical: [1, 4,7] [2,5,8] [3,6,9]
+// Diagonal: [1,5,9] [3,5,7]
+// Array stores the combinations
+var winners = new Array();
+
+function loadAnswers() {
+  winners.push([1, 2, 3]);
+  winners.push([4, 5, 6]);
+  winners.push([7, 8, 9]);
+  winners.push([1, 4, 7]);
+  winners.push([2, 5, 8]);
+  winners.push([3, 6, 9]);
+  winners.push([1, 5, 9]);
+  winners.push([3, 5, 7]);
 }
